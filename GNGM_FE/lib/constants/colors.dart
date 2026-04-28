53c251586c318329bd61a38e6f549d9bbc14e591
chhong <pnci1029@gmail.com>
2026-04-28 20:31:32 +0900

    [FE] 셋업

---
+++ GNGM_FE/lib/constants/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GNGMColors {
+  // Primary Colors
+  static const Color trustBlue = Color(0xFF2E86C1);
+  static const Color warmOrange = Color(0xFFF39C12);
+  
+  // Secondary Colors
+  static const Color successGreen = Color(0xFF27AE60);
+  static const Color urgentRed = Color(0xFFE74C3C);
+  static const Color cleanWhite = Color(0xFFFFFFFF);
+  static const Color softGray = Color(0xFF95A5A6);
+  static const Color lightGray = Color(0xFFECF0F1);
+  
+  // Text Colors
+  static const Color primaryText = Color(0xFF2C3E50);
+  static const Color secondaryText = Color(0xFF7F8C8D);
+  static const Color hintText = Color(0xFFBDC3C7);
+  
+  // Background Colors
+  static const Color backgroundColor = cleanWhite;
+  static const Color cardBackground = lightGray;
+  static const Color surfaceColor = Color(0xFFF8F9FA);
+}
---
+++ GNGM_FE/lib/widgets/category_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../constants/colors.dart';
+import '../constants/text_styles.dart';
+
+class CategoryItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final VoidCallback onTap;
+
+  const CategoryItem({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: GNGMColors.cleanWhite,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 32,
+              color: GNGMColors.trustBlue,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              title,
+              style: GNGMTextStyles.bodyMedium.copyWith(
+                color: GNGMColors.primaryText,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
