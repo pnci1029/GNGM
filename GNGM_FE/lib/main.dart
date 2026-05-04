429f46bc2de7c5f70fe3b0498c5c8d39f835c7b7
chhong <pnci1029@gmail.com>
2026-05-04 16:11:45 +0900

    [FE] 추가된 화면 적용

--- GNGM_FE/lib/main.dart
+++ GNGM_FE/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'constants/colors.dart';
 import 'constants/text_styles.dart';
 import 'screens/home/home_screen.dart';
 import 'screens/category/category_screen.dart';
 import 'screens/request/request_detail_screen.dart';
 import 'screens/request/create_request_screen.dart';
+import 'screens/auth/login_screen.dart';
+import 'providers/auth_provider.dart';
+import 'providers/request_provider.dart';
+import 'providers/offer_provider.dart';
+import 'services/api_client.dart';
+import 'services/auth_service.dart';
+import 'services/request_service.dart';
+import 'services/offer_service.dart';
 
 void main() {
   runApp(const GNGMApp());
@@ -15,39 +24,56 @@ class GNGMApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GNGM',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-        scaffoldBackgroundColor: AppColors.background,
-        fontFamily: 'Pretendard',
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.primary,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
+    final apiClient = ApiClient();
+    final authService = AuthService(apiClient);
+    final requestService = RequestService(apiClient);
+    final offerService = OfferService(apiClient);
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider(authService, apiClient)),
+        ChangeNotifierProvider(create: (_) => RequestProvider(requestService)),
+        ChangeNotifierProvider(create: (_) => OfferProvider(offerService)),
+      ],
+      child: Consumer<AuthProvider>(
+        builder: (context, authProvider, child) {
+          return MaterialApp(
+            title: 'GNGM',
+            theme: ThemeData(
+              primaryColor: AppColors.primary,
+              scaffoldBackgroundColor: AppColors.background,
+              fontFamily: 'Pretendard',
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: AppColors.primary,
+                brightness: Brightness.light,
+              ),
+              useMaterial3: true,
+            ),
+            home: authProvider.isLoggedIn ? const HomeScreen() : const LoginScreen(),
+            routes: {
+              '/home': (context) => const HomeScreen(),
+              '/login': (context) => const LoginScreen(),
+              '/create-request': (context) => const CreateRequestScreen(),
+            },
+            onGenerateRoute: (settings) {
+              if (settings.name == '/category') {
+                final categoryType = settings.arguments as String;
+                return MaterialPageRoute(
+                  builder: (context) => CategoryScreen(categoryType: categoryType),
+                );
+              }
+              if (settings.name == '/request-detail') {
+                final requestId = settings.arguments as String;
+                return MaterialPageRoute(
+                  builder: (context) => RequestDetailScreen(requestId: requestId),
+                );
+              }
+              return null;
+            },
+            debugShowCheckedModeBanner: false,
+          );
+        },
       ),
-      home: const HomeScreen(),
-      routes: {
-        '/home': (context) => const HomeScreen(),
-        '/create-request': (context) => const CreateRequestScreen(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == '/category') {
-          final categoryType = settings.arguments as String;
-          return MaterialPageRoute(
-            builder: (context) => CategoryScreen(categoryType: categoryType),
-          );
-        }
-        if (settings.name == '/request-detail') {
-          final requestId = settings.arguments as String;
-          return MaterialPageRoute(
-            builder: (context) => RequestDetailScreen(requestId: requestId),
-          );
-        }
-        return null;
-      },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
